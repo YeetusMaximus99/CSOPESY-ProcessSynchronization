7237a73f391088c8e5ef2ccc445c546f50923417
YeetusMaximus99 <77537993+YeetusMaximus99@users.noreply.github.com>
2023-04-17 10:14:13 +0800

    Fixed Thread id print

--- procsync.py
+++ procsync.py
@@ -5,6 +5,7 @@ logging.basicConfig(level=logging.DEBUG,
                     format='(%(threadName)-9s) %(message)s',)
 class FittingRoom(threading.Thread):
     def __init__(self, n,blue,green):
+        threading.Thread.__init__(self)
         self.slots = n
         self.blue_count = 0
         self.green_count = 0
@@ -19,7 +20,7 @@ class FittingRoom(threading.Thread):
         self.total_blue = blue
         self.past_color= None
         self.next_color = None
-    def enter(self):
+    def enter(self,id):
             with self.lock:
                 self.sem2.acquire()
                 if self.current_color is None:
@@ -41,7 +42,7 @@ class FittingRoom(threading.Thread):
                     self.total_green -=1
             
                 self.current_count += 1
-                print(f"{threading.get_ident()} - {self.current_color}")
+                print(f"{id} - {self.current_color}")
                 if self.current_count == self.slots or (self.total_green == 0 and self.current_color == 'green') or (self.total_blue == 0 and self.current_color == 'blue'):  
                         self.past_color = self.current_color
                         if self.current_color == "blue" and self.total_green > 0:
@@ -63,16 +64,16 @@ class FittingRoom(threading.Thread):
                 self.sem2.release()
  
 
-def run(color, count, fitting_room):
+def run(color, count, fitting_room,id):
     for i in range(count):
-        fitting_room.enter()
+        fitting_room.enter(id)
 def main():
     slots = int(input("Enter number of slots inside the fitting room: "))
     blue = int(input("Enter number of Blues:"))
     green = int(input("Enter number of Green:"))
     fitting_room = FittingRoom(slots,blue,green)
-    blue_threads = [threading.Thread(target=run, args=("blue", 1, fitting_room)) for i in range(blue)]
-    green_threads = [threading.Thread(target=run, args=("green", 1, fitting_room)) for i in range(green)]
+    blue_threads = [threading.Thread(target=run, args=("blue", 1, fitting_room,i)) for i in range(blue)]
+    green_threads = [threading.Thread(target=run, args=("green", 1, fitting_room,blue+i)) for i in range(green)]
     threads = blue_threads + green_threads
     for thread in threads:
         thread.start()
