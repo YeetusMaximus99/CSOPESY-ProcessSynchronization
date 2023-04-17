1b08d870605fac552df5b86c1d3d67efe8e32603
YeetusMaximus99 <77537993+YeetusMaximus99@users.noreply.github.com>
2023-04-17 09:48:47 +0800

    Added Sempahores

--- procsync.py
+++ procsync.py
@@ -3,12 +3,14 @@ import logging
 
 logging.basicConfig(level=logging.DEBUG,
                     format='(%(threadName)-9s) %(message)s',)
-class FittingRoom:
+class FittingRoom(threading.Thread):
     def __init__(self, n,blue,green):
         self.slots = n
         self.blue_count = 0
         self.green_count = 0
         self.lock = threading.Lock()
+        self.sem = threading.BoundedSemaphore(n)
+        self.sem2 = threading.BoundedSemaphore(n)
         self.blue_condition = threading.Condition(self.lock)
         self.green_condition = threading.Condition(self.lock)
         self.current_color = None
@@ -18,50 +20,48 @@ class FittingRoom:
         self.past_color= None
         self.next_color = None
     def enter(self):
-        with self.lock:
-            if self.current_color is None:
-                self.current_color = "blue"
-                print(f"{self.current_color} only")
-            if self.current_color == "blue" and self.green_count == 0:
-                while self.current_color != None and self.green_count > 0:
-                    self.blue_condition.wait()
-                self.blue_count += 1
-                self.total_blue -=1
-                while self.current_color == "green":
-                     self.green_condition.wait()
-            if self.current_color == "green" and self.blue_count ==0:
-                while self.current_color != None and self.blue_count > 0:
+            with self.lock:
+                self.sem2.acquire()
+                if self.current_color is None:
+                    self.current_color = "blue"
+                    print(f"{self.current_color} only")
+                if self.current_color == "blue" and self.green_count == 0:
+                    while self.current_color != None and self.green_count > 0:
+                        self.blue_condition.wait()
+                    while self.current_color == "green":
                         self.green_condition.wait()
-                while self.current_color == "blue":
-                     self.blue_condition.wait()
-                self.green_count += 1
-                self.total_green -=1
-
-                    
+                    self.blue_count += 1
+                    self.total_blue -=1    
+                if self.current_color == "green" and self.blue_count ==0:
+                    while self.current_color != None and self.blue_count > 0:
+                            self.green_condition.wait()
+                    while self.current_color == "blue":
+                        self.blue_condition.wait()
+                    self.green_count += 1
+                    self.total_green -=1
             
-            self.current_count += 1
-            print(f"{threading.get_ident()} - {self.current_color}")
-    def leave(self):
-        with self.lock:
-            if self.current_count == self.slots or (self.total_green == 0 and self.current_color == 'green') or (self.total_blue == 0 and self.current_color == 'blue'):  
+                self.current_count += 1
+                print(f"{threading.get_ident()} - {self.current_color}")
+                if self.current_count == self.slots or (self.total_green == 0 and self.current_color == 'green') or (self.total_blue == 0 and self.current_color == 'blue'):  
                         self.past_color = self.current_color
-                        if self.current_color == "blue":
+                        if self.current_color == "blue" and self.total_green > 0:
                             self.current_color = "green"
-                            self.green_condition.notify_all()
-                        elif self.current_color == "green":
-                            self.current_color = "blue"
-                            self.blue_condition.notify_all()
+                            self.green_condition.notify_all()     
+                        elif self.current_color == "green" and self.total_blue > 0:
+                            self.current_color = "blue" 
+                            self.blue_condition.notify_all()       
                         self.current_count = 0
                         self.blue_count = 0
                         self.green_count = 0
-                        
+                          
                         
                         print("Empty fitting room.") 
+                self.sem2.release()
+ 
+
 def run(color, count, fitting_room):
     for i in range(count):
         fitting_room.enter()
-    for i in range(count):
-        fitting_room.leave()
 def main():
     slots = int(input("Enter number of slots inside the fitting room: "))
     blue = int(input("Enter number of Blues:"))
